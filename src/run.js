42db02fe420ebaef010c6ca906818160e909317f
e.fedotov <fedotov1290@gmail.com>
2020-05-02 14:30:27 +0300

    feat: use config from package.json

--- src/run.js
+++ src/run.js
@@ -7,17 +7,31 @@ const { existsSync, writeFileSync } = require("fs");
 const { execSync } = require("child_process");
 const { version } = require("../package.json");
 
+const defaultOutputDir = "./api";
+
 program
   .version(version)
   .option("--file <path>", "Swagger JSON file with api")
-  .option("--output-dir <path>", "Output directory js api with types", "./api");
+  .option(
+    "--output-dir <path>",
+    `Output directory js api with types (default: "${defaultOutputDir}")`,
+  );
 
 program.parse(process.argv);
 
-if (program.file) {
+const pathPackageJson = path.resolve(process.cwd(), "package.json");
+const { swaggerapi: swaggerapiConfig = {} } = require(pathPackageJson) || {};
+const config = {
+  file: program.file ? program.file : swaggerapiConfig.file,
+  outputDir:
+    (program.outputDir ? program.outputDir : swaggerapiConfig.outputDir) ||
+    defaultOutputDir,
+};
+
+if (config.file) {
   // Paths
-  const pathFile = path.resolve(process.cwd(), program.file);
-  const pathOutputDir = path.resolve(process.cwd(), program.outputDir);
+  const pathFile = path.resolve(process.cwd(), config.file);
+  const pathOutputDir = path.resolve(process.cwd(), config.outputDir);
   const pathIndex = path.resolve(pathOutputDir, "index.js");
   const pathTypes = path.resolve(pathOutputDir, "index.d.ts");
 
@@ -32,5 +46,5 @@ if (program.file) {
   writeFileSync(pathIndex, paths.code);
   writeFileSync(pathTypes, `${definitions}\n// Methods\n${paths.types}`);
 } else {
-  throw new Error("Insert path to file with swagger api");
+  throw new Error("Setup path to file with swagger api");
 }
