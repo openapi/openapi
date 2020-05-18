f8aef420074c31c2d92d703d75844f18c3fb2136
e.fedotov <fedotov1290@gmail.com>
2020-05-18 18:59:20 +0300

    feat(common): build object by refs, by mode

--- src/common/build-object-by-refs.js
+++ src/common/build-object-by-refs.js
@@ -1,92 +1,27 @@
 const { getRef } = require("../lib/get-ref");
+const { isObject } = require("../lib/is-object");
+const { rebuildObject } = require("../lib/rebuild-object");
 
-function buildObjectByRefs(mode, object, state) {
-  let result = { ...object };
-  const ref = checkGetRef(result, state);
+function buildObjectByRefs(object, state) {
+  if (isObject(object)) {
+    const ref = checkGetRef(object, state);
+    let next = { ...object };
 
-  if (ref) {
-    result = {
-      ...buildObjectByRefs(mode, ref, state),
-      ...result,
-    };
-  } else if (result.schema) {
-    result = buildObjectByRefs(mode, result.schema, state);
-  } else if (result.type) {
-    result = buildObjectTypeByType(mode, result, state);
-  } else if (typeof result === "object") {
-    result = buildObject(mode, result, state);
-  }
-
-  return result;
-}
-
-function buildObjectTypeByType(mode, object, state) {
-  switch (object.type) {
-    case "object":
-      return buildObjectTypeHowObject(mode, object, state);
-    case "array":
-      return buildObjectTypeHowArray(mode, object, state);
-    default:
-      return object;
-  }
-}
-
-function buildObjectTypeHowObject(mode, object, state) {
-  if (object.additionalProperties) {
-    object.additionalProperties = buildObjectByRefs(
-      mode,
-      object.additionalProperties,
-      state,
-    );
-  } else if (object.properties) {
-    object.properties = buildObject(mode, object.properties, state);
-
-    if (mode === "xml") {
-      object = xmlObjectWrapper(object);
+    if (ref) {
+      delete next["$ref"];
+      next = { ...buildObjectByRefs(ref, state), ...next };
+    } else if (next.schema) {
+      next.schema = buildObjectBySchema(next.schema, state);
+    } else if (next.type) {
+      next = buildObjectTypeByType(next, state);
+    } else {
+      next = rebuildObject(next, (value) => buildObjectByRefs(value, state));
     }
+
+    return next;
   }
 
   return object;
-}
-
-function buildObjectTypeHowArray(mode, object, state) {
-  if (object.items) {
-    object.items = buildObjectByRefs(mode, object.items, state);
-
-    if (mode === "xml" && object.items.xml) {
-      object = xmlObjectWrapper(object.items, (items) => {
-        const ref = checkGetRef(items, state);
-        const nextItems = ref ? { ...ref } : items;
-
-        delete nextItems.xml;
-
-        return {
-          type: "array",
-          items: nextItems,
-        };
-      });
-    }
-  }
-
-  return object;
-}
-
-function buildObject(mode, object, state) {
-  return Object.keys(object).reduce((memo, key) => {
-    let value = object[key];
-
-    if (mode === "xml") {
-      const ref = checkGetRef(value, state);
-
-      if (ref) {
-        value = { ...ref };
-        delete value.xml;
-      }
-    }
-
-    memo[key] = buildObjectByRefs(mode, value, state);
-    return memo;
-  }, {});
 }
 
 function checkGetRef(object, state) {
@@ -97,15 +32,42 @@ function checkGetRef(object, state) {
   return null;
 }
 
-// xml
-function xmlObjectWrapper(object, callback = (value) => value) {
-  if (object.xml && object.xml.name) {
-    const propName = object.xml.name;
+function buildObjectBySchema(object, state) {
+  return {
+    type: "object",
+    ...buildObjectByRefs(object, state),
+  };
+}
 
-    return {
-      type: "object",
-      properties: { [propName]: callback(object) },
-    };
+function buildObjectTypeByType(object, state) {
+  switch (object.type) {
+    case "object":
+      return buildObjectTypeHowObject(object, state);
+    case "array":
+      return buildObjectTypeHowArray(object, state);
+    default:
+      return object;
+  }
+}
+
+function buildObjectTypeHowObject(object, state) {
+  if (object.additionalProperties) {
+    object.additionalProperties = buildObjectByRefs(
+      object.additionalProperties,
+      state,
+    );
+  } else if (object.properties) {
+    object.properties = rebuildObject(object.properties, (value) =>
+      buildObjectByRefs(value, state),
+    );
+  }
+
+  return object;
+}
+
+function buildObjectTypeHowArray(object, state) {
+  if (object.items) {
+    object.items = buildObjectByRefs(object.items, state);
   }
 
   return object;
